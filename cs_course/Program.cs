dced44238ae68f38f4ee32fea34ae1b7ef5104e2
Edward Greenwood <edwardgreenwood38@gmail.com>
2019-07-31 13:52:11 -0400

    initial commit

---
+++ cs_course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello {0}!");
+        }
+    }
+}
