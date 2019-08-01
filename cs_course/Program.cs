03cf4c52e4eb3673d83923aeb6bd91205e63c08c
Edward Greenwood <edwardgreenwood38@gmail.com>
2019-08-01 08:23:43 -0400

    end ch2

--- cs_course/Program.cs
+++ cs_course/Program.cs
@@ -6,7 +6,14 @@ namespace cs_course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {0}!");
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
