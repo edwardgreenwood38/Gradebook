6269c0bed75ccd3bfb957919bf8452f7dc4c8f58
Edward Greenwood <edwardgreenwood38@gmail.com>
2019-08-01 14:58:01 -0400

    end ch4

---
+++ cs_course/Book.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace cs_course
+{
+    class Book
+    {
+        public Book(string name) //contructor
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade) //method
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStats()
+        {
+            double result = 0.0;
+            double highGrade = double.MinValue;
+            double lowGrade = double.MaxValue;
+
+            foreach(double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+        }  
+
+        private List<double> grades; // property
+        private string name;        
+    }
+}
--- cs_course/Program.cs
+++ cs_course/Program.cs
@@ -7,27 +7,17 @@ namespace cs_course
     {
         static void Main(string[] args)
         { 
-            double result;
-            List<double> grades = new List<double>() {12.75, 56.23, 32.1, 4.19};
-            grades.Add(27.9);
 
-            result = 0.0;
-            foreach(double number in grades)
-            {
-                result += number;
-            }
+            Book book = new Book("Ed's grade book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
 
-            Console.WriteLine($"The average grade is {result / grades.Count:N1}");
+            book.ShowStats();
+
             
+
             
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello World!");
-            }
         }
     }
 }
